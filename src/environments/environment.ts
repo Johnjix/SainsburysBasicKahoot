fb957d4ea1fcdad55ed918b42a0fd575540b7bf7
John Jix <John_jix@hotmail.co.uk>
2021-12-06 23:57:11 +0000

    Added Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'sainsburysbasickahoot',
+    appId: '1:2627640310:web:8da6aa0e3a9771360345d7',
+    databaseURL: 'https://sainsburysbasickahoot-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'sainsburysbasickahoot.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyAxCDhTxAwioK03pis5c4S2Lk451wReq2E',
+    authDomain: 'sainsburysbasickahoot.firebaseapp.com',
+    messagingSenderId: '2627640310',
+    measurementId: 'G-YNRDNRWEYW',
+  },
   production: false
 };
 
